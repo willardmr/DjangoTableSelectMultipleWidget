6b73b07d485dbba242fccef04e041126a2f81d6a
Petr Dlouhý <petr.dlouhy@email.cz>
2017-04-17 17:05:57 +0200

    bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def read(fname):
 
 setup(
     name="django-table-select-widget",
-    version="0.0.2",
+    version="1.0.0",
     description=read('DESCRIPTION'),
     long_description=read('README.rst'),
     platforms=['OS Independent'],
